feb53424bd977604320288c81bed5c26d705d3ff
Jacob Harris <jacobwho07@gmail.com>
2026-07-06 21:27:02 -0500

    added game context

---
+++ src/GameContext.jsx
@@ -0,0 +1,58 @@
+import { createContext, useContext, useState } from "react";
+
+function randomMole() {
+  return Math.floor(Math.random() * 9);
+}
+
+export function GameProvider({ children }) {
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [score, setScore] = useState(0);
+    const [moleIndex, setMoleIndex] = useState(0);
+
+
+    function startGame() {
+        setIsPlaying(true);
+        setScore(0);
+        setMoleIndex(randomMole());
+
+    }
+
+    function restartGame() {
+        setIsPlaying(false)
+        setScore(0);
+    }
+
+    function whackMole() {
+        setScore((currentScore) => currentScore + 1);
+        setMoleIndex(randomMole());
+    }
+
+      return (
+    <GameContext.Provider
+      value={{
+        isPlaying,
+        score,
+        moleIndex,
+        startGame,
+        restartGame,
+        whackMole,
+      }}
+    >
+      {children}
+    </GameContext.Provider>
+  );
+}
+
+
+export function useGameContext() {
+  const context = useContext(GameContext);
+
+  if (context === null) {
+    throw new Error("useGameContext must be used within a GameProvider");
+  }
+
+  return context;
+}
+
+
+
